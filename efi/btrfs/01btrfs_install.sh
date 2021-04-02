ca77b57504bf8a5ae174dcdb741adafdd5fc4c27
Le GymHueck <42337056+legymhueck@users.noreply.github.com>
2021-04-02 09:48:03 +0200

    Update 01btrfs_install.sh

--- efi/btrfs/01btrfs_install.sh
+++ efi/btrfs/01btrfs_install.sh
@@ -16,7 +16,20 @@ mkdir /mnt/{boot,home}
 mount -o ssd,compress=zstd:1,noatime,subvol=@home /dev/sda2 /mnt/home
 mount /dev/sda1 /mnt/boot
 
-pacstrap /mnt base linux linux-headers linux-firmware efibootmgr sudo vim networkmanager btrfs-progs xdg-user-dirs
+pacstrap /mnt base linux linux-headers linux-firmware efibootmgr sudo vim btrfs-progs xdg-user-dirs
 
 genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "[Match]
+Name=en*
+
+[Network]
+DHCP=yes
+#Address=192.168.2.11/24
+#Gateway=192.168.2.1
+#DNS=192.168.2.3
+" > /etc/systemd/network/20-wired.network
+
+ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
 arch-chroot /mnt
