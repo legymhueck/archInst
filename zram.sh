bc010fb68c482f8c01e5f9e18c41862d22166919
Le GymHueck <42337056+legymhueck@users.noreply.github.com>
2021-04-03 15:43:09 +0200

    Update zram.sh

--- zram.sh
+++ zram.sh
@@ -2,4 +2,4 @@
 echo "zram" > /etc/modules-load.d/zram.conf
 echo "options zram num_devices=2" > /etc/modprobe.d/zram.conf
 echo "KERNEL=="zram0", ATTR{disksize}="512M" RUN="/usr/bin/mkswap /dev/zram0", TAG+="systemd"" > /etc/udev/rules.d/99-zram.rules
-echo "/etc/fstab" >> /etc/fstab
+echo "/dev/zram0 none swap defaults 0 0" >> /etc/fstab
