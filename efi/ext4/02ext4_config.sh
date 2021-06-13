7cb737b0d4523ce2c14f87ab5ff65c34f7c568a4
Le GymHueck <42337056+legymhueck@users.noreply.github.com>
2021-06-13 09:27:52 +0200

    Update 02ext4_config.sh

--- efi/ext4/02ext4_config.sh
+++ efi/ext4/02ext4_config.sh
@@ -1,7 +1,7 @@
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 hwclock --systohc
 
-sed -i '/de_DE.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
+# sed -i '/de_DE.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
 sed -i '/en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
 
 locale-gen
@@ -13,22 +13,6 @@ echo "arch" > /etc/hostname
 echo "127.0.0.1 localhost
 ::1       localhost
 127.0.1.1 arch.localdomain  arch" > /etc/hosts
-
-echo "LANG=en_US.UTF-8
-LANGUAGE=en_US
-LC_CTYPE="de_DE.UTF-8"
-LC_NUMERIC="de_DE.UTF-8"
-LC_TIME="de_DE.UTF-8"
-LC_COLLATE="de_DE.UTF-8"
-LC_MONETARY="de_DE.UTF-8"
-LC_MESSAGES="de_DE.UTF-8"
-LC_PAPER="de_DE.UTF-8"
-LC_NAME="de_DE.UTF-8"
-LC_ADDRESS="de_DE.UTF-8"
-LC_TELEPHONE="de_DE.UTF-8"
-LC_MEASUREMENT="de_DE.UTF-8"
-LC_IDENTIFICATION="de_DE.UTF-8"
-LC_ALL=" > /etc/locale.conf
 
 passwd
 
